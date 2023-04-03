65cf3cb5f63133e5595a548d12f26b286fb85144
Mikhail Lavrov <none@none.none>
2023-04-04 01:19:58 +0300

    added names to login and password fields. changed their orientation

--- app/src/main/java/com/kmt/passwordmanager/passwordsScreen/PasswordsFragment.kt
+++ app/src/main/java/com/kmt/passwordmanager/passwordsScreen/PasswordsFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
 import androidx.databinding.DataBindingUtil
 import com.kmt.passwordmanager.R
 import com.kmt.passwordmanager.database.AuthInfoDatabase
